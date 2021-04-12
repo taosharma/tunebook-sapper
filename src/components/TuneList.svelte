d7d0ca729e99d5d4e5ddc7905ab111243581580f
Tao Sharma <tao@schoolofcode.co.uk>
2021-04-12 18:49:45 +0100

    basic list

--- src/components/TuneList.svelte
+++ src/components/TuneList.svelte
@@ -1,4 +1,10 @@
 <script>
     import tunebook from "../../data/tunebook"
-    console.log(tunebook)
 </script>
+
+<ol>
+    {#each tunebook as {name, orderId}}
+    <li class="hover:text-red-900">{orderId}. {name}</li>
+    {/each}
+</ol>
+
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -4,10 +4,6 @@
 
 <GlobalStyle />
 
-<p>See me above the counter.</p>
-
 <main>
   <slot />
 </main>
-
-<p>See me below the counter.</p>
